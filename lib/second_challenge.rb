3462762ee073c0251a312274432ca691307e98af
Jooyon Won <jyon1204@gmail.com>
2019-05-07 02:31:22 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,16 +5,28 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :hero => {},
+<<<<<<< HEAD
       :hero_friends => []
+=======
+      :hero_friends => {}
+>>>>>>> 80b33f534ad1a61dd75ad583090cb79b304cd7b8
       },
     :capulet => {
       #your key/value pairs here!
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
+<<<<<<< HEAD
       :heroine_friends => []
       }
       }
 
 
+=======
+      :heroine_friends => {}
+      },
+      }}
+
+
+>>>>>>> 80b33f534ad1a61dd75ad583090cb79b304cd7b8
 end
